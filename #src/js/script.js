741c013852e53dfd0eb024d29bc3e0a792b42bc7
Andrey <stuff-box@bk.ru>
2021-10-12 11:31:09 +0300

    Init

--- #src/js/script.js
+++ #src/js/script.js
@@ -13,19 +13,20 @@ function burgerMenu(selector) {
 	//у ссылок и подложки будет просто вызываться функция toggleMenu
 	links.on('click', () => toggleMenu());
 	overlay.on('click', () => toggleMenu());
-	console.log(links);
-	console.log(overlay);
-
+	
 	//создадим функцию, которая будет отвечать за переключение меню из активного состояния в неактивное
 	function toggleMenu() {
 		//сначала будем либо добавлять класс active к нашему меню
 		topmenu.toggleClass('active');
 		//сделаем так,чтобы когда меню активно - было нельзя прокручивать область контента
-		if (topmenu.hasClass('top__menu active')) {
+		if (topmenu.hasClass('top__menu wrap active')) {
 			$('body').css('overflow','hidden');
-			//$('body').addClass('body__hidden');
+			overlay.addClass('active');
 		} else {
 			$('body').css('overflow','visible');
+			setTimeout(function() {
+				overlay.removeClass("active");
+		  	}, 300);
 		}
 	}
 }
